3cee5c9b6e1c5ed07ac052d84df1213c6f7a0051
Guillaume DEDRIE <guillaume.dedrie+git@gmail.com>
2013-03-10 18:46:47 +0100

    Add flashbag; add captcha

--- src/Watcher/WatchBundle/Controller/DefaultController.php
+++ src/Watcher/WatchBundle/Controller/DefaultController.php
@@ -3,19 +3,39 @@
 namespace Watcher\WatchBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('WatcherWatchBundle:Default:main.html.twig');
-    }
+  public function indexAction(Request $request)
+  {
+
+    return $this->render('WatcherWatchBundle:Default:main.html.twig');
+  }
 
 
-    public function voteAction($id)
-    {
-        return $this->render('WatcherWatchBundle:Default:vote.html.twig', array('id' => $id));
+  public function voteAction($id)
+  {
+    $form = $this->createFormBuilder()
+      ->add('id','hidden', array('data' => $id))
+      ->add('identifiant', 'text', array('label' => 'Identifiant Ensimag'))
+      ->add('mdp', 'password', array('label' => 'Mot de passe Ensimag'))
+      ->add('captcha','captcha')
+      ->getForm();
+
+    if ($this->getRequest()->isMethod('POST')) {
+      $form->bind($this->getRequest());
+      if ($form->isValid()) {
+            // effectuez quelques actions, comme sauvegarder la tâche dans
+            // la base de données
+	  $this->get('session')->getFlashBag()->add('notice','Your changes were saved!');
+      }
+      return $this->redirect($this->generateUrl('watcher_watch_homepage'));
     }
+
+    return $this->render('WatcherWatchBundle:Default:vote.html.twig', array('form' => $form->createView(), 'id' => $id));
+  }
 
 
 }
